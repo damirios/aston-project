b1ea9d3947cf27bc216532332ba40f658c1822bc
damirios <damirdigaz@gmail.com>
2022-11-15 14:26:29 +0500

    Добавил логотип

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link, useLocation } from "react-router-dom";
 export function Header() {
     let location = useLocation();
 
-    console.log(location);
+    console.log(location.pathname);
 
     return (
         <header className="header">
             <div className="container">
                 <div className="header__row">
-                    <Link to='/'>
-                        Game!
+                    <Link to='/' className="header__logo">
+                        <img src="./assets/images/Logo.png" alt="Logo" />
                     </Link>
                 </div>
             </div>
